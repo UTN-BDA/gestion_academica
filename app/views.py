1b45339b8641ebe13c2a59ff20db8064e1e98106
matias <matias31.5.03alvarado@gmail.com>
2025-05-10 14:44:53 -0300

    add templates y modelo a terminar de vista de inicio

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    return HttpResponse("HOME DJANGO")
+
+# VISTA DE INICIO falta terminar
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+# VISTAS PARA Usuario
+
+
+    
+
+# VISTAS para Carreras
+
+# VISTAS    
