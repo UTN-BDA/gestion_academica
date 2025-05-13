2e88119d323db0be307829010c850b4ad1583043
matias <matias31.5.03alvarado@gmail.com>
2025-05-12 22:47:55 -0300

    changes in web site

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 
+from django.contrib import messages
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -8,7 +10,9 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            return redirect('home')  # Asegurate de tener una vista llamada 'home'
+            messages.success(request, 'Inicio de sesión exitoso')  # Agregar mensaje de éxito
+            return redirect('home')  # Redirigir a la página principal
         else:
             return render(request, 'usuarios/login.html', {'error': 'Credenciales inválidas.'})
     return render(request, 'usuarios/login.html')
+
