17ec10ff18eabdc480cd03a15edf2c3d0641394d
dbeagus <dbeagus@gmail.com>
2025-05-14 12:39:37 -0300

    home view for user and admin

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name='usuarios'
 urlpatterns = [
     path('', views.default, name='default'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('user/home/', views.user_home, name='user_home'),
+    path('administration/home/', views.admin_home, name='admin_home'),
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -13,10 +13,17 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            messages.success(request, 'Inicio de sesión exitoso')  # Agregar mensaje de éxito
-            return redirect('home')  # Redirigir a la página principal
+            messages.success(request, 'Inicio de sesión exitoso')
+            if user.rol != 'admin':
+                return redirect('usuarios:user_home')
+            return redirect('usuarios:admin_home')
         else:
             messages.error(request, 'Credenciales invalidas')
             return render(request, 'login.html')
     return render(request, 'login.html')
 
+def user_home(request):
+    return render(request, 'user_home.html', {})
+
+def admin_home(request):
+    return render(request, 'admin_home.html', {})
