5558ba467fbfdcaff02e98f4a24c34b71390724f
matias <matias31.5.03alvarado@gmail.com>
2025-07-22 20:06:40 -0300

    add panel_admin

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
-
+from usuarios.models import User  
+from materias.models import Materia, Career  
 from django.contrib import messages
 
 def default(request):
@@ -39,4 +40,19 @@ def user_home(request):
 
 @login_required
 def admin_home(request):
-    return render(request, 'admin_home.html', {})
+    if request.user.rol != 'admin':
+        return redirect('usuarios:user_home')
+
+    total_usuarios = User.objects.count()
+    total_materias = Materia.objects.count()
+    total_carreras = Career.objects.count()
+    ultimos_usuarios = User.objects.order_by('-date_joined')[:5]  # o '-id' si no tenés date_joined
+
+    context = {
+        'total_usuarios': total_usuarios,
+        'total_materias': total_materias,
+        'total_carreras': total_carreras,
+        'ultimos_usuarios': ultimos_usuarios,
+    }
+
+    return render(request, 'usuarios/admin_home.html', context)
